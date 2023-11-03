99586511bba00b196ca2302c03cf5dd6003aabc4
RumenMoysev <rumoysev@gmail.com>
2023-11-03 18:58:16 +0200

    Add routes and userController

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const routes = require('./routes.js')
 
 const app = express()
 
 const SERVER_PORT = 3030
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.use(routes)
 
 app.listen(SERVER_PORT, () => console.log(`REST server is listening on port: ${SERVER_PORT}`))
